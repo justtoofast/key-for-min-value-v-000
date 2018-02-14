6c4ac4b70de4cd0aa226a397a4aa43dde5f4b0a6
Trac Nguyen <nguyendtrac@gmail.com>
2018-02-14 19:20:44 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min_key = {}
-  name_hash.collect do |key, value|
-
+  if name_hash == {}
+    return nil
+  else
+  min_key = ""
+  min_value = name_hash.first[1]
+  name_hash.each do |key, value|
+      if value < min_value
+        min_value = v
+      end
+      min_value = key if name_hash[key] == min_value
   end
 end
